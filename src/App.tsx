ac8be45d4c42714c351636e65f5c54f293bf67b0
José Carlos <josecarlosbcn@gmail.com>
2025-04-25 19:53:49 +0200

    Add GitHub Actions deploy

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Aquí, el deploy lo ha hecho github, por lo que después del push no tienes que hacer nada más.
         </p>
       </div>
       <p className="read-the-docs">
